9e2adb1f3168ff2a86d89482c8cbdb29909cab75
janagassan <janassrallah@gmail.com>
2026-06-20 19:20:50 +0300

    Update emailService.js

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -11,6 +11,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 30000,
+greetingTimeout: 30000,
+socketTimeout: 30000,
 });
 
 export const sendVerificationEmail = async (to, code) => {
